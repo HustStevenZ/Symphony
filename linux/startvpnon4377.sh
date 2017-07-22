1d53980f3861193edcbda96493a35c58ecf70b9d
Sanqian Zhao <sanqianzhao@gmail.com>
2017-07-22 16:38:02 +0800

    Refine startvpn4377 script

--- linux/startvpnon4377.sh
+++ linux/startvpnon4377.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+ps aux | grep ss-local | grep ssconfig | awk '{print $2}' | xargs kill -9
 decodeqr.sh | xargs decodeqrurl.py >~/App/ssconfig.json
-sslocal -c ~/App/ssconfig.json >/dev/null 2>&1 &
+ss-local -c ~/App/ssconfig.json >/dev/null 2>&1 &
