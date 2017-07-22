c276c2ef4a2eea7e27d2e3d41a3f541ba7cf3097
Sanqian Zhao <sanqianzhao@gmail.com>
2017-07-22 16:36:19 +0800

    add more resource url

--- linux/decodeqr.sh
+++ linux/decodeqr.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+#avaiable proxies 
+#   http://freess.cx/images/servers/us01-03.png
+#   http://freess.cx/images/servers/jp01-03.png
+#   http://ss.ishadowx.com/img/qr/jpa.png
+#   http://ss.ishadowx.com/img/qr/usa.png
+#   
 wget -q  http://freess.org/images/servers/us01.png  -O us01.png
 zbarimg -q us01.png  | sed 's/QR-Code:ss:\/\///' | base64 --decode
