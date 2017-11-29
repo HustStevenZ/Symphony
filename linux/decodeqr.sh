88c0d73a7391c166622c5e5fa6e5a5f32cabf7bc
Sanqian Zhao <sanqianzhao@gmail.com>
2017-11-29 13:57:34 +0800

    add more source

--- linux/decodeqr.sh
+++ linux/decodeqr.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
-#avaiable proxies 
-#   http://freess.cx/images/servers/us01-03.png
-#   http://freess.cx/images/servers/jp01-03.png
-#   http://ss.ishadowx.com/img/qr/jpa.png
-#   http://ss.ishadowx.com/img/qr/usa.png
-#   
-wget -q  http://freess.org/images/servers/us01.png  -O us01.png
+#wget -q  http://freess.cx/images/servers/us01.png  -O us01.png
+#wget -q  http://freess.cx/images/servers/us02.png  -O us01.png
+#wget -q  http://freess.cx/images/servers/us03.png  -O us01.png
+wget -q  http://freess.cx/images/servers/jp01.png  -O us01.png
+#wget -q  http://freess.cx/images/servers/jp02.png  -O us01.png
+#wget -q  http://freess.cx/images/servers/jp03.png  -O us01.png
+#http://ss.ishadowx.com/img/qr/usa.png
+
+#wget -q  http://ss.ishadowx.com/img/qr/usa.png  -O us01.png
+#wget -q  http://ss.ishadowx.com/img/qr/usb.png  -O us01.png
+#wget -q  http://ss.ishadowx.com/img/qr/usc.png  -O us01.png
+#wget -q  http://ss.ishadowx.com/img/qr/jpa.png  -O us01.png
+#wget -q  http://ss.ishadowx.com/img/qr/jpb.png  -O us01.png
+#wget -q  http://ss.ishadowx.com/img/qr/jpc.png  -O us01.png
+#wget -q  http://get.shadowsocks8.cc/images/server01.png  -O us01.png
 zbarimg -q us01.png  | sed 's/QR-Code:ss:\/\///' | base64 --decode
